12519cc3d55c6f918ea4b2b272ee2c19199a04ad
Kaique <kvz@certi.org.br>
2025-02-18 11:02:37 -0300

    Fix: update allow cors origins

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+  });
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true
