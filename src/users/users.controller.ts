3dfa85c6c7bb3c3286405cfc6c256bf502896aff
Kaique <kvz@certi.org.br>
2025-02-18 10:11:26 -0300

    Fix: fixing jwt auth guard import

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,27 +3,28 @@ import { UsersService } from './users.service';
 import { CreateUsersDto } from './dto/create-users.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
+
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Get('/')
-  @UseGuards(JwtAuthGuard) 
+  @UseGuards(JwtAuthGuard)
   async findAll() {
     return this.usersService.findAll();
   }
 
   @Get('/check_extra_info')
-  async checkExtraInfo(@Param('email') email: string){
+  async checkExtraInfo(@Param('email') email: string) {
     return this.usersService.checkExtraInfo(email);
   }
 
   @Get(':id')
   @UseGuards(JwtAuthGuard)
-  async findById(@Param('id') id: number){
+  async findById(@Param('id') id: number) {
     return this.usersService.findById(id);
   }
-  
+
 
   @Post()
   async create(@Body(ValidationPipe) createUsersDto: CreateUsersDto) {
