30894c707baa0a67d3b9419c207e9e5d92aaa9b8
Kaique <kvz@certi.org.br>
2025-02-18 11:34:30 -0300

    update: allowedOrigins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 // import * as cookieParser from 'cookie-parser';
 import cookieParser from 'cookie-parser';
 
-const allowedOrigins = [process.env.FRONTEND_URL];
+const allowedOrigins = ["https://n88-frontend.vercel.app", "https://www.n88-frontend.vercel.app", "http://localhost:3000"];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
