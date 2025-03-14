487c371841d74ad1c9e986485222a7ad9eb24348
gustavobftorres <gutobevtorres@gmail.com>
2025-03-14 16:59:53 -0300

    add fiz on import googleOauthConfig

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -4,7 +4,7 @@ import { ConfigService, ConfigType } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { UsersService } from '../../users/users.service';
-import googleOauthConfig from 'src/config/google-oauth.config';
+import googleOauthConfig from '../../config/google-oauth.config';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
