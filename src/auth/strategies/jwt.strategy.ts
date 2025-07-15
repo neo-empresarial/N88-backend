7ad08a48062386b7a5755c0e233e44c1832f685c
Gustavo Torres <gustavotorres@Gustavos-MacBook-Pro.local>
2025-07-15 18:16:40 -0300

    add debug to the backend, so i can track what is happening with jwt

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,25 +1,26 @@
-﻿import { PassportStrategy } from "@nestjs/passport";
-import { Request } from "express";
-import { ExtractJwt, Strategy } from "passport-jwt";
+﻿import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 import { config as dotenvConfig } from 'dotenv';
-import { TokenPayload } from "../auth-payload.interface";
-import { UsersService } from "src/users/users.service";
-import { Injectable } from "@nestjs/common";
+import { TokenPayload } from '../auth-payload.interface';
+import { UsersService } from 'src/users/users.service';
+import { Injectable, Logger } from '@nestjs/common';
 dotenvConfig({ path: '.env' });
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  private readonly logger = new Logger(JwtStrategy.name);
+
   constructor(private readonly usersService: UsersService) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request.cookies?.Authentication
-      ]),
-      secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_ACCESS_TOKEN_SECRET,
     });
   }
 
-  async validate(payload: TokenPayload){
-    return this.usersService.findById(payload.userId);
+  async validate(payload: any) {
+    this.logger.debug('JWT Payload:', payload);
+    return payload;
   }
 }
