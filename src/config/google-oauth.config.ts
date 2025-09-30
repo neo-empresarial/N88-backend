bfa43f3a9494de2ee9cfefeabaf844faa24d2b8f
JTR <orchulhak.ufsc@gmail.com>
2025-09-30 14:39:55 -0300

    change url for google auth

--- src/config/google-oauth.config.ts
+++ src/config/google-oauth.config.ts
@@ -3,5 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('googleOAuth', () => ({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
+  callbackURL: `${process.env.BACKEND_URL}auth/google/callback`,
 }));
