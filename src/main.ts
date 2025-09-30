3272f5dd50fc53f9738451d85677ccdc378a7f72
JTR <orchulhak.ufsc@gmail.com>
2025-09-30 14:45:52 -0300

    add deploy cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// import cookieParser from 'cookie-parser'; // para deploy na vercel
-import * as cookieParser from 'cookie-parser'; // local
+import cookieParser from 'cookie-parser'; // para deploy na vercel
+// import * as cookieParser from 'cookie-parser'; // local
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
