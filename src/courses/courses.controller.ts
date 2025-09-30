4314ea4505266568dcbae761529e8807f726166d
JTR <orchulhak.ufsc@gmail.com>
2025-09-30 10:39:48 -0300

    change cookieparser

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,13 +1,6 @@
-import {
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { Courses } from './courses.entity';
 import { CoursesService } from './courses.service';
-import { JwtAuthGuard } from 'src/auth/guards/local-auth.guard';
 //import { UpdateSubjectsDto } from './dto/update-subjects.dto'; possivelmente terei que fazer um desses para cursos
 
 @Controller('courses')
@@ -23,7 +16,6 @@ export class CoursesController {
   async findByName(@Query('name') name: string): Promise<Courses[]> {
     return this.coursesService.findByName(name);
   }
-
 
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<Courses> {
