c2bb4f7c4db2fd4c77b276d62286b378b26c2654
Gustavo Torres <gustavotorres@Gustavos-MacBook-Pro.local>
2025-11-08 16:08:44 -0300

    fix google auth config to corectly redirect user for handshake

--- src/config/google-oauth.config.ts
+++ src/config/google-oauth.config.ts
@@ -3,5 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('googleOAuth', () => ({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.BACKEND_URL}auth/google/callback`,
+  callbackURL: process.env.BACKEND_URL
+    ? `${process.env.BACKEND_URL}auth/google/callback`
+    : `http://localhost:8000/auth/google/callback`,
 }));
