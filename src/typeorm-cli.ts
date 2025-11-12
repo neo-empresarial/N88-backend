abebb36169da9466bb5b08544ee8d44987b00ab3
Gustavo Torres <gustavotorres@Gustavos-MacBook-Pro.local>
2025-11-12 14:32:09 -0300

    create migration commands to local db deploy

--- src/typeorm-cli.ts
+++ src/typeorm-cli.ts
@@ -1,18 +1,17 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
-const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT ? parseInt(process.env.TYPEORM_PORT, 10) : 5432,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['src/migrations/*.ts'],
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const isProd = (process.env.NODE_ENV ?? 'development') === 'production';
+const useSSL = /^true$/i.test(process.env.DATABASE_SSL || '');
 
-export default AppDataSource;
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST || 'postgres',
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'n88prd',
+  entities: isProd ? ['dist/**/*.entity.js'] : ['src/**/*.entity.ts'],
+  migrations: isProd ? ['dist/migrations/*.js'] : ['src/migrations/*.ts'],
+  ssl: useSSL ? { rejectUnauthorized: false } : false,
+});
