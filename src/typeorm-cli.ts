aa0653c6c73849315d9a566b8e2d6a2830c7cb63
Gustavo Torres <gustavotorres@Gustavos-MacBook-Pro.local>
2025-11-12 15:23:53 -0300

    fix typo on typeorm cli file

--- src/typeorm-cli.ts
+++ src/typeorm-cli.ts
@@ -1,10 +1,9 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
-const isProd = (process.env.NODE_ENV ?? 'development') === 'production';
-const useSSL = /^true$/i.test(process.env.DATABASE_SSL || '');
+const isProd = process.env.NODE_ENV === 'production';
 
-export default new DataSource({
+const dataSource = new DataSource({
   type: 'postgres',
   host: process.env.DATABASE_HOST || 'postgres',
   port: Number(process.env.DATABASE_PORT) || 5432,
@@ -13,5 +12,7 @@ export default new DataSource({
   database: process.env.DATABASE_NAME || 'n88prd',
   entities: isProd ? ['dist/**/*.entity.js'] : ['src/**/*.entity.ts'],
   migrations: isProd ? ['dist/migrations/*.js'] : ['src/migrations/*.ts'],
-  ssl: useSSL ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
+
+export default dataSource;
